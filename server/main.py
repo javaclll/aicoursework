323b1e37dfc50cc5e01bbbb854409311878e9fa1
javaclll <yugu.upadhyaya@gmail.com>
2023-12-29 02:00:00 +0545

    compeleted: endpoints to serve data

--- server/main.py
+++ server/main.py
@@ -16,24 +16,124 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-@app.get("/")
-def read_root():
-    return {"Hello": "World"}
-
 @app.get("/eightpuzzle/bfs")
 def bfs():
     searching = EightPuzzle.Search(
             EightPuzzle.Node(
                 EightPuzzle.Game(
-                    EightPuzzle.State([1, 2, 3, 4, 5, 6, 7, " ", 8]),
+                    EightPuzzle.State([1, 2, 3, 4, 8, " ", 7, 6, 5]),
                     EightPuzzle.State([1, 2, 3, 4, 5, 6, 7, 8, " "]),
                 )
             )
         )
        
-    depth = 2
-        # temp = searching.start_search_idfs(depth)
-        # temp = searching.start_search_misplaced()
     temp = searching.start_single_search_bfs()
+
+    return{"Searching Status": searching.finished, "Found": str(temp), "Data": searching.graphvizzes}
+
+@app.get("/eightpuzzle/dfs")
+def dfs():
+    searching = EightPuzzle.Search(
+            EightPuzzle.Node(
+                EightPuzzle.Game(
+                    EightPuzzle.State([1, 2, 3, 4, 8, " ", 7, 6, 5]),
+                    EightPuzzle.State([1, 2, 3, 4, 5, 6, 7, 8, " "]),
+                )
+            )
+        )
+       
+    # if(temp is None):
+    #         print(f"Cannot find the end state within {depth} depth.")
+    # else:
+    #     while len(temp.parent) != 0:
+    #         print(temp)
+    #         temp = temp.parent[0]
+
+    #     print(f"root: {searching.root}")
+    #     print(searching.finished)
+
+    temp = searching.start_single_search_dfs()
+
+    return{"Searching Status": searching.finished, "Found": str(temp), "Data": searching.graphvizzes}
+
+@app.get("/eightpuzzle/idfs/{depth}")
+def idfs(depth: int = 3):
+    searching = EightPuzzle.Search(
+            EightPuzzle.Node(
+                EightPuzzle.Game(
+                    EightPuzzle.State([1, 2, 3, 4, 8, " ", 7, 6, 5]),
+                    EightPuzzle.State([1, 2, 3, 4, 5, 6, 7, 8, " "]),
+                )
+            )
+        )
+       
+    temp = searching.start_search_idfs(depth)
+
+    if(temp is None):
+            print(f"Cannot find the end state within {depth} depth.")
+    else:
+        while len(temp.parent) != 0:
+            print(temp)
+            temp = temp.parent[0]
+
+        print(f"root: {searching.root}")
+        print(searching.finished)
+
+    # temp = searching.start_single_search_dfs()
+
+    return{"Searching Status": searching.finished, "Found": str(temp), "Data": searching.graphvizzes}
+
+
+@app.get("/eightpuzzle/heuristics/manhatten")
+def manhatten():
+    searching = EightPuzzle.Search(
+            EightPuzzle.Node(
+                EightPuzzle.Game(
+                    EightPuzzle.State([1, 2, 3, 4, 8, " ", 7, 6, 5]),
+                    EightPuzzle.State([1, 2, 3, 4, 5, 6, 7, 8, " "]),
+                )
+            )
+        )
+       
+    temp = searching.start_search_manhatten()
+
+    # if(temp is None):
+    #         print(f"Cannot find the end state within {depth} depth.")
+    # else:
+    #     while len(temp.parent) != 0:
+    #         print(temp)
+    #         temp = temp.parent[0]
+
+    #     print(f"root: {searching.root}")
+    #     print(searching.finished)
+
+    # temp = searching.start_single_search_dfs()
+
+    return{"Searching Status": searching.finished, "Found": str(temp), "Data": searching.graphvizzes}
+
+@app.get("/eightpuzzle/heuristics/misplaced")
+def misplaced():
+    searching = EightPuzzle.Search(
+            EightPuzzle.Node(
+                EightPuzzle.Game(
+                    EightPuzzle.State([1, 2, 3, 4, 8, " ", 7, 6, 5]),
+                    EightPuzzle.State([1, 2, 3, 4, 5, 6, 7, 8, " "]),
+                )
+            )
+        )
+       
+    temp = searching.start_search_misplaced()
+
+    # if(temp is None):
+    #         print(f"Cannot find the end state within {depth} depth.")
+    # else:
+    #     while len(temp.parent) != 0:
+    #         print(temp)
+    #         temp = temp.parent[0]
+
+    #     print(f"root: {searching.root}")
+    #     print(searching.finished)
+
+    # temp = searching.start_single_search_dfs()
 
     return{"Searching Status": searching.finished, "Found": str(temp), "Data": searching.graphvizzes}
